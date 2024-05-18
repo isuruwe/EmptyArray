999d083b5c126160647235fd04c157b1e56b9a9d
Isuru Weerasinghe <isuruwe@gmail.com>
2024-05-18 17:12:30 +0530

    v43456789fdgg

--- src/EmptyArray.java
+++ src/EmptyArray.java
@@ -1,52 +1,53 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class EmptyArray extends Exception {}
+public class EmptyArray extends Exception {
 
-float avg(int[] nums) throws EmptyArray, NegativeNumberFoundException {
-    int sum = 0;
-    if (nums == null || nums.length == 0)
-        throw new EmptyArray();
+    float avg(int[] nums) throws EmptyArray, NegativeNumberFoundException {
+        int sum = 0;
+        if (nums == null || nums.length == 0)
+            throw new EmptyArray();
 
-    List<String> negativeIndices = new ArrayList<>();
-    for (int i = 0; i < nums.length; i++) {
-        if (nums[i] < 0) {
-            negativeIndices.add((i + 1) + "-th number " + nums[i]);
+        List<String> negativeIndices = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                negativeIndices.add((i + 1) + "-th number " + nums[i]);
+            }
+            sum += nums[i];
         }
-        sum += nums[i];
-    }
 
-    if (!negativeIndices.isEmpty()) {
-        throw new NegativeNumberFoundException(negativeIndices);
-    }
-
-    return (float) sum / nums.length;
-}
-
-public void main(String[] args) {
-    int[] nums = new int[]{1, 2};
-    Float result = null;
-    try {
-        result = avg(nums);
-    } catch (EmptyArray e) {
-        System.out.println("Array is empty");
-    } catch (NegativeNumberFoundException e) {
-        for (String s : e.getNegativeIndices()) {
-            System.out.println("The " + s + " in your array is invalid");
+        if (!negativeIndices.isEmpty()) {
+            throw new NegativeNumberFoundException(negativeIndices);
         }
+
+        return (float) sum / nums.length;
     }
-    System.out.println("Result: " + result);
 
-
-    int[] numsWithNegatives = new int[]{1, -2, -3, 4};
-    try {
-        result = avg(numsWithNegatives);
-    } catch (EmptyArray e) {
-        System.out.println("Array is empty");
-    } catch (NegativeNumberFoundException e) {
-        for (String s : e.getNegativeIndices()) {
-            System.out.println("The " + s + " in your array is invalid");
+    public void main(String[] args) {
+        int[] nums = new int[]{1, 2};
+        Float result = null;
+        try {
+            result = avg(nums);
+        } catch (EmptyArray e) {
+            System.out.println("Array is empty");
+        } catch (NegativeNumberFoundException e) {
+            for (String s : e.getNegativeIndices()) {
+                System.out.println("The " + s + " in your array is invalid");
+            }
         }
+        System.out.println("Result: " + result);
+
+
+        int[] numsWithNegatives = new int[]{1, -2, -3, 4};
+        try {
+            result = avg(numsWithNegatives);
+        } catch (EmptyArray e) {
+            System.out.println("Array is empty");
+        } catch (NegativeNumberFoundException e) {
+            for (String s : e.getNegativeIndices()) {
+                System.out.println("The " + s + " in your array is invalid");
+            }
+        }
+        System.out.println("Result: " + result);
     }
-    System.out.println("Result: " + result);
 }
